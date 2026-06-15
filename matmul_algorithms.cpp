8f097784d9e43669f30e8693528934ea663f318d
Pedro-Meza-0889 <11087@soyunaq.mx>
2026-06-15 09:57:31 -0600

    Actualizacion:
    matmul_algorithms.cpp

--- matmul_algorithms.cpp
+++ matmul_algorithms.cpp
@@ -5,8 +5,23 @@
 void matmulModSequential(const Matrix* A, const Matrix* B, Matrix* C, int modN)
 {
     /* TODO Limpiar la matriz multiplicación antes de llenarla */
+
+    zeroMatrix(C);          //C es la matriz del resultado de la multiplicacion.
+
     /* TODO : Implementar el algoritmo */
 
+    for (int i = 0; i < A->rows; i++)
+    {
+        for (int j = 0; j < B->cols; j++)
+        {
+            long long sum = 0;
+            for (int k = 0; k < A->cols; k++)
+            {
+                sum += (long long)A->data[i * A->cols + k] * B->data[k * B->cols + j];
+            }
+            C->data[i * C->cols + j] = modNormalize(sum, modN);
+        }
+    }
 }
 
 void matmulModOmpRows(const Matrix* A, const Matrix* B, Matrix* C, int modN)
